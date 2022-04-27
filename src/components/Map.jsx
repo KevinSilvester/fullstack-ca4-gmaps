5609e14c8bdfefc72285da6b34ddd4d95848f037
Kevin Silvester <kevinsilvester05@gmail.com>
2022-04-27 11:10:14 +0100

    tried three js 3d marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,7 +5,14 @@
 
 import React, { useCallback, useMemo, useRef, useState } from 'react'
 import { useJsApiLoader, Marker, GoogleMap } from '@react-google-maps/api'
-import { AmbientLight, DirectionalLight, Scene } from 'three'
+import {
+   AmbientLight,
+   DirectionalLight,
+   Matrix4,
+   PerspectiveCamera,
+   Scene,
+   WebGLRenderer
+} from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { ThreeJSOverlayView } from '@googlemaps/three'
 import MapControl from './MapControl'
@@ -22,7 +29,9 @@ const Map = () => {
    const mapRef = useRef()
 
    const birmingham = useMemo(() => ({ lat: 52.483726, lng: -1.892682 }), [])
-
+   const [displayControls, setDisplayControls] = useState(false)
+   const [coordinates, setCoordinates] = useState({ lat: 52.483726, lng: -1.892682 })
+   
    /** @type {MapOptions} */
    const mapOptions = useMemo(
       () => ({
@@ -30,13 +39,13 @@ const Map = () => {
          disableDefaultUI: true,
          fullscreenControl: true,
          streetViewControl: true,
-         center: birmingham
+         center: birmingham,
+         zoom: 18,
+         tilt: 0,
+         heading: 0
       }),
       []
    )
-
-   const [displayControls, setDisplayControls] = useState(false)
-
 
    /**
     * @param {'tilt' | 'rotate'} mode
@@ -58,7 +67,34 @@ const Map = () => {
    const onMapLoad = useCallback(map => {
       mapRef.current = map
       setDisplayControls(true)
+
+      // const scene = new Scene()
+      // const ambientLight = new AmbientLight(0xffffff, 0.75)
+      // const directionalLight = new DirectionalLight(0xffffff, 0.25)
+      // directionalLight.position.set(0, 10, 50)
+      // scene.add(ambientLight)
+      // scene.add(directionalLight)
+
+      // const gltfLoader = new GLTFLoader()
+      // const url = 'https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf'
+
+      // gltfLoader.load(url, gltf => {
+      //    gltf.scene.scale.set(10, 10, 10)
+      //    gltf.scene.rotation.x = Math.PI / 2
+      //    scene.add(gltf.scene)
+      // })
+
+      // new ThreeJSOverlayView({
+      //    map,
+      //    scene,
+      //    anchor: { ...coordinates, altitude: 100 },
+      //  });
+
    }, [])
+
+   const handleClick = () => {
+      setCoordinates({ lat: Math.random() * 180 - 90, lng: Math.random() * 360 - 180 })
+   }
 
    if (loadError) {
       return <div>Error loading maps</div>
@@ -70,16 +106,16 @@ const Map = () => {
 
    return (
       <div className='map__wrapper'>
+         <button onClick={() => handleClick()}> CHANGE CENTER</button>
          <GoogleMap
             mapContainerClassName='map__container'
-            zoom={10}
-            center={birmingham}
             options={mapOptions}
             onLoad={onMapLoad}
+            center={coordinates}
          >
             {displayControls && (
                <>
-                  <Marker position={birmingham} />
+                  <Marker position={coordinates} />
                   <MapControl position='TOP_CENTER' onClick={() => adjustMap('tilt', -10)}>
                      Tilt Up
                   </MapControl>
